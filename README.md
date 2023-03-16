f1dd601299a556da40d6858c0aa184016d85d8d6
dheerajstvpm <103765896+dheerajstvpm@users.noreply.github.com>
2023-03-16 18:24:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-workout-app
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8rmsnf)
---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './test/test.component.html',
+})
+export class App {
+  name = 'Angular';
+
+  arr2 = [{ a: '' }, { b: '' }, { d: '' }];
+  arr1 = [{ a: 1 }, { b: 1 }, { c: 1 }];
+  arr3: any = [];
+
+  mergeArrays(arr1: any, arr2: any) {
+    const mergedArray = arr1.concat(arr2);
+
+    const result = mergedArray.reduce((accumulator: any, currentValue: any) => {
+      const index = accumulator.findIndex(
+        (item: any) => Object.keys(item)[0] === Object.keys(currentValue)[0]
+      );
+      if (index === -1) {
+        accumulator.push(currentValue);
+      }
+      return accumulator;
+    }, []);
+
+    return result;
+  }
+  ngOnInit() {
+    this.arr3 = this.mergeArrays(this.arr1, this.arr2);
+    console.log(this.arr3);
+  }
+}
+
+bootstrapApplication(App);
